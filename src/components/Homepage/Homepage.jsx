e9bb13e9fec96f2aacdc82507518f937130a68e8
Generalsoi <ikhinobelesuccess@gmail.com>
2022-03-01 14:52:33 +0100

    added the dashboard

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import "./Homepage.css";
-import Sidebar from "../Sidebar/Sidebar";
 
 const Homepage = () => {
-  return (
-    <div className="homepage">
-      <div className="sidebar">
-        <Sidebar />
-      </div>
-      <div className="content">Content</div>
-    </div>
-  );
+  return <div>Homepage</div>;
 };
 
 export default Homepage;
