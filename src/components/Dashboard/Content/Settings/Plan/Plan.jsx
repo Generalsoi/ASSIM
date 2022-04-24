009bdbca1eb8cd60ae0a7db4d42cf76c8de83352
Generalsoi <ikhinobelesuccess@gmail.com>
2022-04-24 22:05:13 +0100

    'updates'

--- src/components/Dashboard/Content/Settings/Plan/Plan.jsx
+++ src/components/Dashboard/Content/Settings/Plan/Plan.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Plan.css";
 
 const Plan = () => {
-  return <div>Plan</div>;
+  return (
+    <div className="plan">
+      <div className="plan-heading">
+        <h5>Plan</h5>
+        <p>Manage your plans</p>
+      </div>
+    </div>
+  );
 };
 
 export default Plan;
