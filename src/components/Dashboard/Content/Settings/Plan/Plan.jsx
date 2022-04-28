af0a34c3d455b0c66ac7a14e3063ea7f450bea40
Generalsoi <ikhinobelesuccess@gmail.com>
2022-04-28 06:26:40 +0100

    updates to the assim site

--- src/components/Dashboard/Content/Settings/Plan/Plan.jsx
+++ src/components/Dashboard/Content/Settings/Plan/Plan.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Plan.css";
 import FreePlanIcon from "../../../../../assets/images/freeplanicon.png";
+import StandardPlanIcon from "../../../../../assets/images/standardplanicon.png";
+import PremiumPlanIcon from "../../../../../assets/images/premiumplanicon.png";
 
 const Plan = () => {
   return (
@@ -10,12 +12,54 @@ const Plan = () => {
         <p>Manage your plans</p>
       </div>
 
-      <div className="free-plan">
+      <div className="plan-content">
         <div>
           <div>
             <img src={FreePlanIcon} alt="" />
-            <h5>Free Plan</h5>
           </div>
+          <h5>Free Plan</h5>
+        </div>
+        <div>
+          <p>
+            <span>$0</span> per month
+          </p>
+          <p>
+            Includes up to 10 courses and access to download only 10 files per
+            month.
+          </p>
+        </div>
+      </div>
+
+      <div className="plan-content">
+        <div>
+          <div>
+            <img src={StandardPlanIcon} alt="" />
+          </div>
+          <h5>Standard Plan</h5>
+        </div>
+        <div>
+          <p>
+            <span>$10</span> per month
+          </p>
+          <p>
+            Includes up to 20 courses and access to download 20 courses per
+            month
+          </p>
+        </div>
+      </div>
+
+      <div className="plan-content">
+        <div>
+          <div>
+            <img src={PremiumPlanIcon} alt="" />
+          </div>
+          <h5>Premium Plan</h5>
+        </div>
+        <div>
+          <p>
+            <span>$40</span> per month
+          </p>
+          <p>Unlimited courses and access to download all courses.</p>
         </div>
       </div>
     </div>
