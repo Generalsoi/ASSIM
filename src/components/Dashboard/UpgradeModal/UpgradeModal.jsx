33646eb060b2ac8dd485859cc6bc87a67816c458
Generalsoi <ikhinobelesuccess@gmail.com>
2022-05-05 00:57:11 +0100

    updates

--- src/components/Dashboard/UpgradeModal/UpgradeModal.jsx
+++ src/components/Dashboard/UpgradeModal/UpgradeModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./UpgradeModal.css";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import Computer from "../../../assets/images/Computer.png";
 
 const UpgradeModal = (props) => {
   const { setUpgradeModal } = props;
@@ -11,6 +12,10 @@ const UpgradeModal = (props) => {
         <div className="modal-content">
           <div className="close-modal">
             <CloseOutlinedIcon onClick={() => setUpgradeModal(false)} />
+          </div>
+          <div className="modal-content-heading">
+            <img src={Computer} alt="" />
+            <h4>Upgrade Your membership</h4>
           </div>
         </div>
       </div>
