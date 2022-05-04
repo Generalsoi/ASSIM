8e6bc87173a3303fbb978f365aadd7f1a85a33db
Generalsoi <ikhinobelesuccess@gmail.com>
2022-05-04 23:23:06 +0100

    updates

--- src/components/Dashboard/Content/Settings/Billing/Billing.jsx
+++ src/components/Dashboard/Content/Settings/Billing/Billing.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Billing.css";
 
 const Billing = () => {
-  return <div>Billing</div>;
+  return (
+    <div className="billing">
+      <div className="billing-heading">
+        <h5>Billing</h5>
+        <p>Manage your billing and payment details.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Billing;
