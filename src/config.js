35aa1fac9f0874fd4201881bab4f1205953ec780
Generalsoi <ikhinobelesuccess@gmail.com>
2022-05-21 19:39:29 +0100

    updates

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const apiEndpoint = "https://assim-dev.azurewebsites.net/api/v1";
+const apiEndpoint = "https://assim-dev.azurewebsites.net";
 const accessToken = "MK8yB6jh2uEG3xHGB9Bpsde8N3R44Iaf";
 
 export { apiEndpoint, accessToken };
